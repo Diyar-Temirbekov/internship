6d69003aaa05ab9247390c1ef8470b45cdb9644e
Batir Kenjaev <batya.kenjaev@gmail.com>
2025-02-19 16:35:55 +0500

    create project

---
+++ admin/src/main/java/mn/partners/admin/AdminApplication.java
@@ -0,0 +1,7 @@
+package mn.partners.admin;
+
+public class AdminApplication {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ runtime/src/main/java/mn/partners/runtime/RuntimeApplication.java
@@ -0,0 +1,7 @@
+package mn.partners.runtime;
+
+public class RuntimeApplication {
+    public static void main(String[] args) {
+
+    }
+}
